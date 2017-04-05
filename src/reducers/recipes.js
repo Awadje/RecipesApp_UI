b266dfa021af9a36ec82bc9e997dbdbe4ed7fd47
Awadje <michaelawad85@gmail.com>
2017-04-05 15:19:00 +0200

    Add Recipe Action and Reducer

--- src/reducers/recipes.js
+++ src/reducers/recipes.js
@@ -1,19 +1,25 @@
 // src/reducers/recipes.js
 import { SEED_RECIPES } from '../actions/recipes/seed'
 import { TOGGLE_LIKE } from '../actions/recipes/toggleLike'
+import { CREATE_RECIPE } from '../actions/recipes/create'
 
 export default (state = [], { type, payload } = {}) => {
   switch(type) {
     case SEED_RECIPES :
       return [].concat(payload)
 
-    case TOGGLE_LIKE :
-      return state.map((recipe) => {
-        if (recipe._id === payload) {
-          return { ...recipe, liked: !recipe.liked }
-        }
-        return recipe
-      })
+  case TOGGLE_LIKE :
+    return state.map((recipe) => {
+      if (recipe._id === payload) {
+        return { ...recipe, liked: !recipe.liked }
+      }
+      return recipe
+    })
+
+  export default (state = initialState, { type, payload } = {}) => {
+    switch(type) {
+      case CREATE_RECIPE :
+        return [Object.assign({}, payload)].concat(state)
 
     default :
       return state
